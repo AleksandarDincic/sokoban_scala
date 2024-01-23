5e2246a861f76940c632f266e420b8a4e3d231d6
adinc <a.dincic99@gmail.com>
2024-01-23 22:17:52 +0100

    Introduced game state class

--- src/main/scala/sokoban/Map.scala
+++ src/main/scala/sokoban/Map.scala
@@ -6,14 +6,81 @@ import scala.util.{Failure, Success, Try, Using}
 
 class Map(val tilesMatrix: Array[Array[Tile]]) {
 
+  private class GameState(val players: Int, val playerPosition: Option[(Int, Int)], val misplacedCrates: Int, val openTargets: Int, val placedCrates: Int) {
+    def isValid: Try[Unit] = {
+      if (players != 0) {
+        Failure(new Throwable("There should be exactly one player on the map."))
+      } else if (misplacedCrates != openTargets) {
+        Failure(new Throwable("The number of crates and targets on the map should be the same."))
+      }
+      else Success(())
+    }
+
+    def isWon: Boolean = misplacedCrates == 0 && openTargets == 0
+  }
+
+  private def calculateGameState(): GameState = {
+    @tailrec
+    def calculateGameStateTail(row: Int, col: Int, currState: GameState): GameState = {
+      if (row >= tilesMatrix.length) {
+        currState
+      }
+      else {
+        val nextCoords = if ((col + 1) >= tilesMatrix(0).length) {
+          (row + 1, 0)
+        }
+        else {
+          (row, col + 1)
+        }
+
+        val currTile = tilesMatrix(row)(col)
+        val nextGameState = currTile match {
+          case Crate(standingOn) =>
+            standingOn match {
+              case Floor() => new GameState(currState.players, currState.playerPosition, currState.misplacedCrates + 1, currState.openTargets, currState.placedCrates)
+              case Target() => new GameState(currState.players, currState.playerPosition, currState.misplacedCrates, currState.openTargets, currState.placedCrates + 1)
+            }
+          case Player(_) => new GameState(currState.players + 1, Some((row, col)), currState.misplacedCrates, currState.openTargets, currState.placedCrates)
+          case Target() => new GameState(currState.players, Some((row, col)), currState.misplacedCrates, currState.openTargets + 1, currState.placedCrates)
+          case _ => currState
+        }
+
+        calculateGameStateTail(nextCoords._1, nextCoords._2, nextGameState)
+      }
+    }
+
+    calculateGameStateTail(0, 0, new GameState(0, None, 0, 0, 0))
+  }
+
+  private val gameState = calculateGameState()
+
+  val mapWidth: Int = tilesMatrix(0).length
+  val mapHeight: Int = tilesMatrix.length
+
+
+  def movePlayer(direction: Char): Map = ???
+
+  def isValid: Try[Unit] = {
+    val mapIsValid: Try[Unit] = Success(()) //TODO Implement map validation
+    mapIsValid match {
+      case Failure(e) => Failure(e)
+      case Success(_) =>
+        val gameStateIsValid = gameState.isValid
+        gameStateIsValid
+    }
+  }
+
+  def isWon: Boolean = gameState.isWon
+
+  def move(direction: Int): Boolean = ???
 
   override def toString: String = {
     val buffer: StringBuilder = new StringBuilder()
 
     buffer += '\n'
 
-    for(row <- tilesMatrix) {
-      for(tile <- row) {
+    for (row <- tilesMatrix) {
+      for (tile <- row) {
         buffer += tile.symbol
       }
       buffer += '\n'
